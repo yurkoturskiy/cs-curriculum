35abca1928b4e72a789765160a7dd9403975f1e5
Yurko Turskiy <guandjoy@gmail.com>
2020-05-06 22:51:56 +0300

    Add remove method in HashTable data structure

--- TypeScript/Data Structures/HashTable.ts
+++ TypeScript/Data Structures/HashTable.ts
@@ -1,5 +1,7 @@
 /* Hash Table */
 
+import { stringifyJSON } from "fp-ts/lib/Either";
+
 const hash = (string: string, max: number) => {
   // Basic/Simple Hash Function
   const hash: number = string
@@ -11,9 +13,10 @@ const hash = (string: string, max: number) => {
 console.log(hash("somestring", 4));
 
 interface HashTable {
-  storage: [any, any][][];
+  storage: [string, any][][];
   storageLimit: number;
-  add(key: any, value: any): void;
+  add(key: string, value: any): void;
+  remove(key: string): void;
 }
 
 class HashTable implements HashTable {
@@ -22,7 +25,7 @@ class HashTable implements HashTable {
     this.storageLimit = 4;
   }
 
-  add(key: any, value: any) {
+  add(key: string, value: any) {
     const bucketIndex = hash(key, this.storageLimit);
     if (this.storage[bucketIndex] === undefined) {
       // Bucket is free, so init it and place key/value pair there
@@ -31,7 +34,7 @@ class HashTable implements HashTable {
       // Bucket has data inside
       let inserted: boolean = false;
       this.storage[bucketIndex] = this.storage[bucketIndex].reduce(
-        (acc: [any, any][], curr: [any, any], i): [any, any][] => {
+        (acc: [string, any][], curr: [string, any], i): [string, any][] => {
           acc[i] = curr[0] === key ? [key, value] : curr;
           inserted = curr !== acc[i];
           return acc;
@@ -41,10 +44,26 @@ class HashTable implements HashTable {
       inserted === false && this.storage[bucketIndex].push([key, value]);
     }
   }
+
+  remove(key: string) {
+    const bucketIndex = hash(key, this.storageLimit);
+    if (this.storage[bucketIndex]) {
+      this.storage[bucketIndex] = this.storage[bucketIndex].reduce(
+        (acc: [string, any][], curr: [string, any], index: number) =>
+          curr[0] === key ? acc : [...acc, curr],
+        []
+      );
+      if (this.storage[bucketIndex].length === 0)
+        delete this.storage[bucketIndex];
+    }
+  }
 }
 
 const hashTableStorage = new HashTable();
 hashTableStorage.add("some", "value");
 hashTableStorage.add("some", "diffvalue"); // Override
 hashTableStorage.add("another", "somuchdifferentvalue");
+console.log(hashTableStorage.storage);
+hashTableStorage.remove("another");
+hashTableStorage.remove("another");
 console.log(hashTableStorage.storage);
