ed6781dcce4ba96272483af7941c37536ed4d2ec
Yurko Turskiy <guandjoy@gmail.com>
2020-05-08 01:56:42 +0300

    Implement add method in Trie data structure

--- TypeScript/Data Structures/Trie.ts
+++ TypeScript/Data Structures/Trie.ts
@@ -1,5 +1,5 @@
 interface TrieNode {
-  keys: object;
+  keys: Map<string, TrieNode>;
   end: boolean;
   setEnd(): void;
   isEnd(): boolean;
@@ -22,7 +22,7 @@ class TrieNode implements TrieNode {
 
 interface Trie {
   root: TrieNode;
-  add(input: any, node: TrieNode): void;
+  add(input: string, node: TrieNode): void;
   isWord(word: string): boolean;
   print(): string[];
 }
@@ -31,4 +31,20 @@ class Trie implements Trie {
   constructor() {
     this.root = new TrieNode();
   }
+
+  add(input: string, node: TrieNode = this.root): void {
+    if (input.length === 0) {
+      node.setEnd();
+      return;
+    } else if (!node.keys.has(input[0])) {
+      node.keys.set(input[0], new TrieNode());
+      return this.add(input.substr(1), node.keys.get(input[0]));
+    } else {
+      return this.add(input.substr(1), node.keys.get(input[0]));
+    }
+  }
 }
+
+const words = new Trie();
+words.add("work");
+console.log(words);
