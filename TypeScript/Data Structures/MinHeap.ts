59e8db455e07d6c6f5893ad7d5aef0f6b9fffeff
Yurko Turskiy <guandjoy@gmail.com>
2020-05-09 20:08:27 +0300

    Implement remove method in MinHeap data structure

--- TypeScript/Data Structures/MinHeap.ts
+++ TypeScript/Data Structures/MinHeap.ts
@@ -3,7 +3,7 @@ type heapType = any[];
 interface MinHeap {
   heap: heapType;
   add(num: number): void;
-  remove(): void;
+  remove(): boolean;
   sort(): number[];
 }
 
@@ -14,6 +14,14 @@ class MinHeap implements MinHeap {
 
   calcParentIndex(index: number): number {
     return Math.floor(index / 2);
+  }
+
+  calcLeftChildIndex(index: number): number {
+    return Math.floor(index * 2);
+  }
+
+  calcRightChildIndex(index: number): number {
+    return Math.floor(index * 2 + 1);
   }
 
   add(num: number) {
@@ -31,6 +39,41 @@ class MinHeap implements MinHeap {
       }
     }
   }
+
+  remove(): boolean {
+    if (this.heap.length < 2) return false;
+    this.heap[1] = this.heap[this.heap.length - 1]; // Del root val and place last instead
+    this.heap.pop(); // Remove last
+    // Validate root number and move down if needed
+    let index = 1;
+    let leftChildIndex = this.calcLeftChildIndex(index);
+    let rightChildIndex = this.calcRightChildIndex(index);
+    let leftChildValue = this.heap[leftChildIndex];
+    let rightChildValue = this.heap[rightChildIndex];
+    while (
+      this.heap[index] > leftChildValue ||
+      this.heap[index] > rightChildValue
+    ) {
+      const minValue =
+        Math.min(leftChildValue, rightChildValue) ||
+        leftChildValue ||
+        rightChildValue;
+      const minValueIndex =
+        minValue === leftChildValue ? leftChildIndex : rightChildIndex;
+      // Replace higher/bigger value with smaller value below
+      [this.heap[index], this.heap[minValueIndex]] = [
+        this.heap[minValueIndex],
+        this.heap[index],
+      ];
+      index = minValueIndex;
+      leftChildIndex = this.calcLeftChildIndex(index);
+      leftChildValue = this.heap[leftChildIndex];
+      rightChildIndex = this.calcRightChildIndex(index);
+      rightChildValue = this.heap[rightChildIndex];
+    }
+    console.log(this.heap);
+    return true;
+  }
 }
 
 let numbers = new MinHeap();
@@ -42,4 +85,8 @@ numbers.add(8);
 numbers.add(9);
 numbers.add(10);
 numbers.add(1);
+console.log(numbers);
+numbers.remove();
+console.log(numbers);
+numbers.remove();
 console.log(numbers);
