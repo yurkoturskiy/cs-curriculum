b6fc3046eeebbecef415def7b5f7d8b5452f395d
Yurko Turskiy <guandjoy@gmail.com>
2020-05-09 19:01:15 +0300

    Implement add method in MinHeap data structure

--- TypeScript/Data Structures/MinHeap.ts
+++ TypeScript/Data Structures/MinHeap.ts
@@ -1,4 +1,4 @@
-type heapType = null[] | number;
+type heapType = any[];
 
 interface MinHeap {
   heap: heapType;
@@ -6,3 +6,40 @@ interface MinHeap {
   remove(): void;
   sort(): number[];
 }
+
+class MinHeap implements MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  calcParentIndex(index: number): number {
+    return Math.floor(index / 2);
+  }
+
+  add(num: number) {
+    this.heap.push(num);
+    if (this.heap.length > 2) {
+      let index = this.heap.length - 1;
+      let parentIndex = this.calcParentIndex(index);
+      while (this.heap[index] < this.heap[parentIndex]) {
+        [this.heap[parentIndex], this.heap[index]] = [
+          this.heap[index],
+          this.heap[parentIndex],
+        ];
+        index = parentIndex;
+        parentIndex = this.calcParentIndex(index);
+      }
+    }
+  }
+}
+
+let numbers = new MinHeap();
+numbers.add(2);
+numbers.add(5);
+numbers.add(3);
+numbers.add(4);
+numbers.add(8);
+numbers.add(9);
+numbers.add(10);
+numbers.add(1);
+console.log(numbers);
