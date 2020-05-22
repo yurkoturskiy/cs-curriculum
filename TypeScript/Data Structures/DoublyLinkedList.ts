8c288e4f554961f2af13ccd65aa6e8ad2a4b1085
Yurko Turskiy <guandjoy@gmail.com>
2020-05-22 15:49:04 +0300

    Implement add method for DoublyLinkedList

--- TypeScript/Data Structures/DoublyLinkedList.ts
+++ TypeScript/Data Structures/DoublyLinkedList.ts
@@ -1,14 +1,14 @@
-interface DoublyLinkedListNode<T> {
-  element: T | null;
-  next: DoublyLinkedListNode<T> | null;
-  prev: DoublyLinkedListNode<T> | null;
+interface DoublyLinkedListNode {
+  element: any;
+  next: DoublyLinkedListNode | null;
+  prev: DoublyLinkedListNode | null;
 }
 
-class DoublyLinkedListNode<T> implements DoublyLinkedListNode<T> {
+class DoublyLinkedListNode implements DoublyLinkedListNode {
   constructor(
-    element: T,
-    prev: DoublyLinkedListNode<T> | null = null,
-    next: DoublyLinkedListNode<T> | null = null
+    element: any,
+    prev: DoublyLinkedListNode | null = null,
+    next: DoublyLinkedListNode | null = null
   ) {
     this.element = element;
     this.prev = prev;
@@ -16,9 +16,9 @@ class DoublyLinkedListNode<T> implements DoublyLinkedListNode<T> {
   }
 }
 
-interface DoublyLinkedList<T> {
-  head: DoublyLinkedListNode<T> | null;
-  tail: DoublyLinkedListNode<T> | null;
+interface DoublyLinkedList {
+  head: DoublyLinkedListNode | null;
+  tail: DoublyLinkedListNode | null;
   length: number;
   clear(): void;
   size(): number;
@@ -26,21 +26,41 @@ interface DoublyLinkedList<T> {
   add<T>(element: T): void; // to the tail O(1)
   addFirst<T>(element: T): void; // to the beginnig O(1)
   addLast<T>(element: T): void;
-  peekFirst(): T;
-  peekLast(): T;
-  removeFirst(): T;
-  removeLast(): T;
-  remove<T>(node: DoublyLinkedListNode<T>): void;
+  peekFirst(): any;
+  peekLast(): any;
+  removeFirst(): any;
+  removeLast(): any;
+  remove(node: DoublyLinkedListNode): void;
   removeAt(index: number): void;
   removeElement<T>(element: T): boolean;
   indexOf<T>(element: T): number;
   contains<T>(element: T): boolean;
 }
 
-class DoublyLinkedList<T> implements DoublyLinkedList<T> {
+class DoublyLinkedList implements DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
+  add<T>(element: T) {
+    const newNode = new DoublyLinkedListNode(element, this.tail);
+    if (!this.head && !this.tail) {
+      this.head = this.tail = new DoublyLinkedListNode(element);
+    } else if (this.tail) {
+      newNode.prev = this.tail;
+      newNode.prev.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
 }
+
+const linkedList = new DoublyLinkedList();
+linkedList.add("something");
+console.log(linkedList);
+linkedList.add("wow");
+linkedList.add("wowII");
+console.log(linkedList);
+console.log(linkedList.tail && linkedList.tail.prev);
