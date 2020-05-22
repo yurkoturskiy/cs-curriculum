d32e3e5c8b6671399a47f12ffd0865eb8d3faf74
Yurko Turskiy <guandjoy@gmail.com>
2020-05-22 19:42:09 +0300

    Rename LinkedList to SinglyLinkedList

--- TypeScript/Algorithms/ReverseSinglyLinkedList.ts
+++ TypeScript/Algorithms/ReverseSinglyLinkedList.ts
@@ -1,4 +1,4 @@
-import { LinkedList } from "../Data Structures/LinkedList";
+import { LinkedList } from "../Data Structures/SinglyLinkedList";
 
 const reverseSinglyLinkedList = (list: LinkedList): LinkedList => {
   // Reverse singly linked list
