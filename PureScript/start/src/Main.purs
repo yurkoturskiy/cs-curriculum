433c9f85990de83c3cc953b11df741e31800f632
Yurko Turskiy <guandjoy@gmail.com>
2020-05-26 23:47:44 +0300

    Try Computing Diagonals with PureScript

--- PureScript/start/src/Main.purs
+++ PureScript/start/src/Main.purs
@@ -1,5 +1,10 @@
 module Main where
 
+import Math (sqrt)
+import Prelude
+
 import Effect.Console
 
-main = log "Hello, World!"
+diagonal w h = sqrt (w * w + h * h)
+
+main = logShow (diagonal 3.0 4.0)
