33569c68668eb040d0ca7b66b94d955a615d7fc0
Yurko Turskiy <guandjoy@gmail.com>
2020-06-01 04:13:33 +0300

    Create selectionSorting.ts

---
+++ TypeScript/Algorithms/sorting/selectionSorting.ts
@@ -0,0 +1,16 @@
+const selectionSorting = (list: number[]): number[] => {
+  const length = list.length;
+  for (let i = 0; i < length; i++) {
+    let min = i;
+    for (let j = i + 1; j < length; j++) {
+      if (list[min] > list[j]) min = j;
+    }
+    if (min !== i) {
+      // Swaping values
+      [list[i], list[min]] = [list[min], list[i]];
+    }
+  }
+  return list;
+};
+
+console.log(selectionSorting([10, -1, 20, 4, -100, 3, 4, 10]));
